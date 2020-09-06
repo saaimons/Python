f623fe457be99fc890f4062af074e97865eb07cc
saaimons <saaimonsg@gmail.com>
2020-09-06 17:16:13 -0300

    Update SQL_pyodbc.py

--- SQL_pyodbc.py
+++ SQL_pyodbc.py
@@ -1,13 +1,13 @@
 import pyodbc
-import datetime
-from debugger import *
-import parserCsvToSql as tablas
 
-class sandbox:
+class sqlconn:
     
     def __init__(self):    
-        self.tag = "sandbox"
-        self.debug = debugger(self.tag)
+        self.tag = "sqlconn"
+        self.min = 0
+        self.x = 1
+        self.errores = []
+        self.dbtablas = []
         self.conn = pyodbc.connect(
             "Driver={SQL Server Native Client 11.0};"
             "Server=127.0.0.1,1433;"
@@ -28,19 +28,12 @@ class sandbox:
         """
         self.cursor = self.conn.cursor()
 
-        """
-        variables generales
-        """
-        self.min = 0
-        self.x = 1
-        self.errores = []
-        self.dbtablas = []
+
 
 
     def checkifExists(self,mTabla):
         '''
         Verificación de existencia de tabla
-        IF EXISTS
         '''
         tag2 = "Check"
         print("/--------------------------/")
@@ -65,122 +58,53 @@ class sandbox:
             verif = False
             tag3= "Tablas"
             try:
-                while verif == False:
-                    for i in rows:
-                        if self.dbtablas[i][2] == mTabla:
-                            print("                 "+str(i)+"  ->  "+self.dbtablas[i][2])
-                            print("     Tabla: "+str(mTabla)+" existe")
-                            #self.dbtablas.clear()
-                            print(":END Check")
-                            print("/--------------------------/")
-                            return True
-                        elif i == rows:
-                            raise Exception()
+                for i in rows:
+                    prev = i
+                    if self.dbtablas[i][2] == mTabla:
+                        print("                 "+str(i)+"  ->  "+self.dbtablas[i][2])
+                        print("     Tabla: "+str(mTabla)+" existe")
+                        #self.dbtablas.clear()
+                        print(":END Check")
+                        print("/--------------------------/")
+                        return True
+            
+                    elif i >= rows:
+                        raise Exception()
             except:
                 print("Ha ocurrido un ERROR")
-                error = str(self.debug.getTime()) +"\nERROR: "+str(tag2)+"->"+str(tag3)+"-> Tabla: "+str(mTabla)+" inexistente."
-                self.errores.append(error)
-                print("                      "+str(tag2)+" "+str(tag3)+"->Tabla: '"+str(mTabla)+"' es inexistente.")
+                error = "\nERROR: "+str(tag2)+"->"+str(tag3)+"-> Tabla: "+str(mTabla)+" inexistente."
+                print("                      "+str(tag2)+" "+str(tag3)+"-> '"+str(mTabla)+"' es inexistente.")
                 print(":END Check")
                 print("/--------------------------/")
                 return False
-        print()
-        
-        print(":END Check")
-        print("/--------------------------/")
-        self.debug.agregarInfo(self.errores)
-        self.debug.start(True,False)
-        
 
-      #  print(" Verificado") if checkifExsits("12345")== True else print (" No Verificado")
-    
-  
-    
-    '''
-    Creacion de tablas
-    CREATE TABLE
-    '''
-    def crearTabla1(self):
-        '''
-        FORMATO 1
-        '''
-        tag2 = "Creacion de Tabla"
-        mTabla = "MiTabla1"
-        try:
-            self.cursor.execute("""CREATE TABLE      
-                            MiTabla1 
-                            (
-                            "id"            INT NOT NULL IDENTITY(1,1),
-                            "Nombre"        VARCHAR(255)     ,
-                            "Descripcion1"  VARCHAR(2048)    ,
-                            "Descripcion2"  VARCHAR(2048)    
-                            )          
-                            """
-                            )
+    def agregarDatos(self,tabla):
+            tablas = parser(tabla)
             
-            self.conn.commit()
-            print("Tabla MiTabla1 Creada Correctamente")
-            return True
-        except:
-            error = str(self.debug.getTime() +"ERROR: "+str(tag2)+"-> Tabla: "+str(mTabla)+" es posible que ya exista.")
-            self.errores.append(error)
-            self.debug.agregarInfo(self.errores)
-            self.debug.start(False,False)
-            return False
-        
-        
+            x = tablas.getColumnas()
+            g = tablas.getValores()
+            n = tablas.n
+            f = tablas.columnas
+            marker = []
+            print(":Comienzo de Agregado de Datos")
+            x = len(f)
+            max = x
             
-    def crearTabla_2(self):
-        '''
-        Create Table
-        '''
-        self.cursor.execute("""CREATE TABLE "TABLA_TEST2" (
-            "id" INT NULL DEFAULT 'NULL',
-            "Nombre" VARCHAR(50) NULL DEFAULT ''NULL',
-            "Descripción1" VARCHAR(255) NULL DEFAULT ''NULL'' ,
-            "Descripción2" VARCHAR(255) NULL DEFAULT ''NULL'' """
-        )
-        self.conn.commit()         
-    
-    def agregarDatos(self):
-        '''
-        Insercion de datos
-        INSERT INTO
-        '''
-        g = tablas.valores
-        c = tablas.columnas
-        marker = []
-        print(":Comienzo de Agregado de Datos")
-        x = len(tablas.columnas)
-        max = x
-        
-        for i in range(tablas.n):
-            self.cursor.execute("""INSERT INTO MiTabla1 (Nombre) VALUES (?)""",["'"+str(g[i])+"'"])
-            self.conn.commit()
-            print("ID "+ str(i)+"----------------------------->Inicio de actualización")
-            for a in range(max): 
-                self.cursor.execute("""UPDATE MiTabla1 set """+str(tablas.columnas[a])+""" = (?) WHERE id ="""+ str(i+1),["'"+str(tablas.valores[a+self.min])+"'"])
+            for i in range(tablas.n):
+                self.cursor.execute("""INSERT INTO """+tabla+""" (Nombre) VALUES (?)""",["'"+str(g[i])+"'"])
                 self.conn.commit()
-                #time.sleep(1)
-                print(str(a)+"  ->  "+str(tablas.columnas[a])+" ->   "+str(tablas.valores[a+self.min]))
-                
-            self.min = (self.min + max)
-            #print("MIN " + str(self.min))
-            print("ID "+ str(i)+"----------------------------->Fin de actualización\n")
-            #print("Fin UPDATE")
-        self.min=0
-        print(":Finn de agregadod de datos")
-        #print("FIN INSERT")
+                print("ID "+ str(i)+"----------------------------->Inicio de actualización")
+                for a in range(max): 
+                    self.cursor.execute("""UPDATE """+tabla+""" set """+str(f[a])+""" = (?) WHERE id ="""+ str(i+1),["'"+str(g[a+self.min])+"'"])
+                    self.conn.commit()
+                    #time.sleep(1)
+                    print(str(a)+"  ->  "+str(f[a])+" ->   "+str(g[a+self.min]))
+                    
+                self.min = (self.min + max)
+                print("ID "+ str(i)+"----------------------------->Fin de actualización\n")
+            self.min=0
+            print(":Finn de agregado de datos")
 
-    def borrarTabla(self,tabla):
-        self.cursor.execute("""drop table """+tabla)
-        self.conn.commit()
-        print("     Tabla: "+str(tabla)+" ha sido eliminada X.X")
-        
-    def clearCache(self):
-        self.dbtablas.clear()
-        self.errores.clear()
-    def cerrarConn(self):
-        self.conn.close()
-        print(":CONEXIÓN CERRADA:")
-        
+
+conexion = sqlconn()
+conexion.checkifExists("tabla_1")
